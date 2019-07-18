e847038ce305574d9cffa793d780275cfdf8027a
devyyj@gmail.com <ryong0906@naver.com>
2019-07-18 16:52:38 +0900

    first commit

---
+++ models/board.js
@@ -0,0 +1,17 @@
+let express = require('express');
+let mongoose = require('mongoose');
+let mai = require('mongoose-auto-increment');
+
+mai.initialize(mongoose.connection);
+
+let schema = new mongoose.Schema({
+    user: String,
+    title:String,
+    content:String,
+    createTime:{type: Date, default: Date.now},
+    updateTime:{type: Date, default: Date.now}
+})
+
+schema.plugin(mai.plugin, {model : 'board', field:'postNumber'} );
+
+module.exports = mongoose.model('board', schema);
---
+++ routes/index.js
@@ -0,0 +1,137 @@
+var express = require('express');
+var passport = require('passport');
+var router = express.Router();
+let board = require('../models/board');
+var Account = require('../models/account');
+
+/* GET home page. */
+router.get('/', function (req, res, next) {
+  board.find(function (err, result) {
+    if (err) {
+      res.render(err);
+    } else {
+      res.render('index', { data: result, user: req.user });
+    }
+  })
+});
+
+router.get('/createPost', function (req, res) {
+  if (req.user == undefined) {
+    res.redirect('/login')
+  } else {
+    res.render('createPost', { data: {} });
+  }
+});
+
+router.post('/createPost', function (req, res) {
+  if (req.query.postNumber != "undefined") {
+    board.updateOne({ postNumber: req.query.postNumber }, {
+      title: req.body.title,
+      content: req.body.content
+    }, function (err, result) {
+      if (err) {
+        res.render(err);
+      } else {
+        console.log(result);
+        res.redirect('/')
+      }
+    })
+  } else {
+    let post = new board({
+      user: req.user.username,
+      title: req.body.title,
+      content: req.body.content
+    });
+
+    post.save(function (err, result) {
+      if (err) {
+        res.render(err);
+      } else {
+        res.redirect('/readPost?postNumber='+result.postNumber)
+      }
+    });
+  }
+});
+
+router.get('/readPost', function (req, res) {
+  board.findOne({ postNumber: req.query.postNumber }, function (err, result) {
+    if (err) {
+      res.render(err);
+    } else {
+      res.render('readPost', { data: result });
+    }
+  });
+});
+
+router.get('/updatePost', function (req, res) {
+  if (req.user == undefined) {
+    res.redirect('/login')
+  } else {
+    board.findOne({ postNumber: req.query.postNumber }, function (err, result) {
+      if (err) {
+        res.render(err);
+      } else {
+        if (result.user != req.user.username) {
+          res.render('warning');
+        } else {
+          res.render('createPost', { data: result });
+        }
+      }
+    });
+  }
+})
+
+router.get('/deletePost', function (req, res) {
+  if (req.user == undefined) {
+    res.redirect('/login')
+  } else {
+    board.findOne({ postNumber: req.query.postNumber }, function (err, result) {
+      if (err) {
+        res.send(err);
+      } else {
+        if (result.user != req.user.username) {
+          res.render('warning');
+        } else {
+          result.delete({ postNumber: req.query.postNumber });
+          res.redirect('/')
+        }
+      }
+    })
+  }
+})
+
+// passport
+router.get('/register', function (req, res) {
+  res.render('register', {});
+});
+
+router.post('/register', function (req, res) {
+  Account.register(new Account({ username: req.body.username }), req.body.password, function (err, account) {
+    if (err) {
+      return res.render('register', { account: account });
+    }
+
+    passport.authenticate('local')(req, res, function () {
+      res.redirect('/');
+    });
+  });
+});
+
+router.get('/login', function (req, res) {
+  res.render('login', { user: req.user });
+});
+
+router.post('/login', passport.authenticate('local'), function (req, res) {
+  res.redirect('/');
+});
+
+router.get('/logout', function (req, res) {
+  req.logout();
+  res.redirect('/');
+});
+
+router.get('/ping', function (req, res) {
+  res.status(200).send("pong!");
+});
+
+module.exports = router;
