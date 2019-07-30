cb3b6843767fd3d3ce56e608291d1c2ab0c7c446
devyyj@gmail.com <ryong0906@naver.com>
2019-07-30 11:44:30 +0900

    회원가입 비밀번호 두번 입력 완료

--- public/javascripts/readPost.js
+++ public/javascripts/readPost.js
@@ -1,24 +1,22 @@
 var xhttp = new XMLHttpRequest()
 
 // eslint-disable-next-line no-unused-vars
-function likePost (disLike = false) {
+function likePost(disLike = false) {
   console.log('call likePost()')
   let ID
-  if (disLike) ID = '#disLike'
-  else ID = '#like'
-
-  if ($(ID).data('user') === undefined) {
+  if (disLike) ID = 'disLike'
+  else ID = 'like'
+  const el = document.getElementById(ID)
+  if (el.getAttribute('data-user') === null) {
     window.location.href = '/login'
-    return
-  }
-
-  xhttp.onreadystatechange = function () {
-    if (this.readyState === 4 && this.status === 200) {
-      alert(this.response)
+  } else {
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        alert(this.response)
+      }
     }
+    const URL = '/likePost?postNumber=' + el.value + '&disLike=' + disLike
+    xhttp.open('GET', URL, true)
+    xhttp.send()
   }
-
-  const URL = '/likePost?postNumber=' + $(ID).data('postnumber') + '&disLike=' + disLike
-  xhttp.open('GET', URL, true)
-  xhttp.send()
 }
--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -1,4 +1,23 @@
-function sameCheck() {
-  console.log(document.getElementById('pw1').value)
-  console.log(document.getElementById('pw2').value)
+// Example starter JavaScript for disabling form submissions if there are invalid fields
+'use strict'
+window.addEventListener('load', function () {
+  // Fetch all the forms we want to apply custom Bootstrap validation styles to
+  var forms = document.getElementsByClassName('needs-validation')
+  // Loop over them and prevent submission
+  Array.prototype.filter.call(forms, function (form) {
+    form.addEventListener('submit', function (event) {
+      if (form.checkValidity() === false || isSame() === false) {
+        event.preventDefault()
+        event.stopPropagation()
+        if (isSame() === false) document.getElementById('feedback').innerHTML = '비밀번호가 일치하지 않읍니다.'
+        else document.getElementById('feedback').innerHTML = ''
+      }
+      form.classList.add('was-validated')
+    }, false)
+  })
+}, false)
+
+function isSame() {
+  if (document.getElementById('pw1').value === document.getElementById('pw2').value) return true
+  else return false
 }
