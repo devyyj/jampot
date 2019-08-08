a86acd93f51b5d1bb67d9d4732d206ee8c252238
devyyj@gmail.com <ryong0906@naver.com>
2019-08-08 15:25:10 +0900

    좋아요 기능 버그 수정

--- public/javascripts/common.js
+++ public/javascripts/common.js
@@ -11,26 +11,26 @@ window.onload = function () {
       }
     }
   }
+}
 
-  // 좋아요 기능
-  function likePost (disLike = false) {
-    var xhttp = new XMLHttpRequest()
-    console.log('call likePost()')
-    let ID
-    if (disLike) ID = 'disLike'
-    else ID = 'like'
-    const el = document.getElementById(ID)
-    if (el.getAttribute('data-user') === null) {
-      $('#askLogin').modal('show')
-    } else {
-      xhttp.onreadystatechange = function () {
-        if (this.readyState === 4 && this.status === 200) {
-          alert(this.response)
-        }
+// 좋아요 기능
+function likePost (disLike = false) {
+  var xhttp = new XMLHttpRequest()
+  console.log('call likePost()')
+  let ID
+  if (disLike) ID = 'disLike'
+  else ID = 'like'
+  const el = document.getElementById(ID)
+  if (el.getAttribute('data-user') === null) {
+    $('#askLogin').modal('show')
+  } else {
+    xhttp.onreadystatechange = function () {
+      if (this.readyState === 4 && this.status === 200) {
+        alert(this.response)
       }
-      const URL = '/likePost?postNumber=' + el.value + '&disLike=' + disLike
-      xhttp.open('GET', URL, true)
-      xhttp.send()
     }
+    const URL = '/likePost?postNumber=' + el.value + '&disLike=' + disLike
+    xhttp.open('GET', URL, true)
+    xhttp.send()
   }
 }
