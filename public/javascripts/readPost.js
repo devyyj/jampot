14f465d0b806e5fcc2d5b70c30a819fbf6b7fb8e
devyyj@gmail.com <ryong0906@naver.com>
2019-08-08 17:01:15 +0900

    첨부 파일 없는 경우 에러 발생 수정

--- public/javascripts/readPost.js
+++ public/javascripts/readPost.js
@@ -9,19 +9,19 @@ if (showOrgEl) {
     const sizeMsg = ' ' + size.toFixed(1) + ' MB'
     showOrgEl.innerHTML = '원본보기' + sizeMsg
   } else showOrgEl.innerHTML = '원본보기'
+
+  // 원본 이미지 보여주기
+  showOrgEl.addEventListener('click', function (event) {
+    const resizeEl = document.getElementById('resize')
+    resizeEl.remove()
+
+    const orgEl = document.createElement('img')
+    orgEl.setAttribute('src', this.value)
+    orgEl.setAttribute('style', 'max-width:100%;')
+
+    const parentEl = document.getElementById('file')
+    parentEl.insertBefore(orgEl, parentEl.childNodes[0])
+
+    this.remove()
+  })
 }
-
-// 원본 이미지 보여주기
-showOrgEl.addEventListener('click', function (event) {
-  const resizeEl = document.getElementById('resize')
-  resizeEl.remove()
-
-  const orgEl = document.createElement('img')
-  orgEl.setAttribute('src', this.value)
-  orgEl.setAttribute('style', 'max-width:100%;')
-
-  const parentEl = document.getElementById('file')
-  parentEl.insertBefore(orgEl, parentEl.childNodes[0])
-
-  this.remove()
-})
